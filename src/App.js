a32c627398f2856d46cf14bcd5d353465aeb770c
Felix Deiters <messismore@users.noreply.github.com>
2021-06-23 14:52:00 +0200

    Add emotion

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { jsx, css } from '@emotion/react/macro'
+import logo from './logo.svg'
+import './App.css'
+
+const style = css`
+  color: hotpink;
+`
+
+const SomeComponent = ({ children }) => (
+  <div css={style}>
+    Some hotpink text.
+    {children}
+  </div>
+)
+
+const anotherStyle = css({
+  textDecoration: 'underline',
+})
+
+const AnotherComponent = () => (
+  <div css={anotherStyle}>Some text with an underline.</div>
+)
+render(
+  <SomeComponent>
+    <AnotherComponent />
+  </SomeComponent>
+)
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <SomeComponent />
+
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -19,7 +46,7 @@ function App() {
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
